07613dfaa5ac92f7f90084b441f5c796c8e2b521
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-05-06 11:53:47 -0300

    dark films

--- src/pages/cadastrar/index.jsx
+++ src/pages/cadastrar/index.jsx
@@ -1,3 +1,5 @@
+import CustomInput from "@/components/customInput";
+import CustomSelect from "@/components/CustomSelect";
 import Header from "@/components/Header";
 import PageWrapper from "@/components/pageWrapper";
 
@@ -8,7 +10,55 @@ export default function Cadastrar(){
                 <h1 className="text-[40px] font-bold text-[#9a86f4]">Adicione um novo filme</h1>
                 <p className="text-[20px] text-gray-500">Preencha o formulário abaixo para adicionar um novo filme à sua nova coleção</p>
                 <div className="w-full flex justify-center pt-[40px]">
-                    <form className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-3"></form>
+                    <form className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-5 gap-4">
+                        <CustomInput 
+                            label="Título"
+                            placeholder="Digite o título do filme"
+                            type="text"
+                        />
+                          <CustomInput 
+                            label="Diretor"
+                            placeholder="Digite o nome do diretor"
+                            type="text"
+                        />
+                        <div className="w-full flex gap-4">
+                            <div className="w-[50%]">
+                                <CustomInput
+                                    label="Ano"
+                                    placeholder="Digite o ano de lançamento"
+                                    type="number"
+                                />    
+                            </div>
+                            <div className="w-[50%] ">
+                                <CustomSelect
+                                    label="Gênero"
+                                    options={["Ação", "Comédia", "Drama", "Terror", "Ficção científica", "Romance"]}
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[30%] items-center flex gap-2 ">
+                           <div className="w-[70%]">
+                                <CustomInput
+                                label="Nota"
+                                placeholder="0"
+                                type="number"
+                                />
+                           </div>
+                            <p className="flex mt-8">0 / 10</p>
+                        </div>
+                        <div className="w-full flex flex-col gap-2">
+                            <label className="text-[17px] font-bold">Sinopse</label>
+                            <textarea className="w-full min-h-[150px] max-h-[150px] rounded-lg border border-[#ffffff1a] focus:border-purple-400 p-2 bg-[#141414] outline-none"></textarea>
+                        </div>
+                        <CustomInput 
+                            label="Banner"
+                            placeholder="URL da imagem"
+                            type="text"
+                        />
+                        <div className="w-full flex justify-end gap-4">
+                            <button type="reset" className="w-[100px] h-[50px] rounded-md bg-[#141414] border border-[#3a364c] cursor-pointer hover:bg-white/10">Cancelar</button>
+                        </div>
+                    </form>
                 </div>
            </div>
        </PageWrapper>
