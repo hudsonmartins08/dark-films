2968119c38a525fb931450a97c41ff4c05f0453a
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-05-07 12:01:04 -0300

    dark films

--- src/components/pageWrapper/index.jsx
+++ src/components/pageWrapper/index.jsx
@@ -2,9 +2,9 @@ import Header from "../Header";
 
 export default function PageWrapper({ children, showButton = true }){
     return(
-        <div className="w-full h-screen flex flex-col ">
+        <div className="w-full h-full flex flex-col ">
             <Header showButton={showButton}/>
-            <div className="w-full h-auto bg-gradient-to-t from-[#020304] to-[#181d29]">
+            <div className="w-full h-auto min-h-screen bg-gradient-to-t from-[#020304] to-[#181d29]">
                 {children}
             </div>
         </div>
