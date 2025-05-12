03a3a8f559cde2fa66628e34589a64ba1775f638
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-05-12 12:09:55 -0300

    dark filmes

--- src/pages/filme/[id].jsx
+++ src/pages/filme/[id].jsx
@@ -1,22 +1,77 @@
 import PageWrapper from "@/components/pageWrapper"
 import { useRouter } from "next/router"
 import { LuArrowLeft } from "react-icons/lu"
+import { BiSolidEdit } from "react-icons/bi";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import instance  from "@/api/instance";
+
 
 export default function Filme(){
     const router = useRouter()
     const {id} = router.query
+    const[filme, setFilme] = useState({})
+    
+    useEffect(() => {
+        async function getFilmeById(){
+            const response = await instance.get(`/api/movies/${id}`)
+            setFilme(response.data)
+        }
+       if(id){
+        getFilmeById()
+       }
+
+    }, [])
+    
 
     return (
         <PageWrapper
         showHeader={false}
         >  
-           <div className="w-full h-full flex flex-col px-[350px] pt-12 items-start">
+           <div className="w-full h-full min-h-screen flex flex-col px-[350px] pt-12 items-start">
                 <button 
                     onClick={() => router.back()}
                     className="px-5 py-2 text-[#8a898c] gap-2 flex items-center justify-center rounded-lg hover:bg-[#27282b] hover:text-[#8f7bd8] ">
                     <LuArrowLeft />
                     <p>Voltar para a lista de filmes</p>
                 </button>
+                <div className="w-full min-h-[90vh] flex ">
+                    <div className="w-[40%] h-full flex flex-col">
+                        <div className="w-full h-[90%] ">
+                            <img
+                                className="w-full h-full rounded-lg object-cover"
+                                src="https://th.bing.com/th/id/OIP.wQNlUkwJYn16LGVQel23KwHaKW?rs=1&pid=ImgDetMain"
+                                alt=""
+                            />
+                        </div>
+                        <div className="w-full flex h-[10%] pt-4 justify-between">
+                            <button className="w-[49%] h-full border border-[#9b87f533] rounded-md text-[#9b87f5] hover:bg-[#9b87f5]/20 flex items-center justify-center gap-2 py-1">
+                            <BiSolidEdit size={15}/>Editar filme</button>
+                            <button className="w-[49%] h-full border border-[#ef444433] rounded-md text-[#ef4444] hover:bg-[#ef4444]/20 flex items-center justify-center gap-2 py-1">
+                            <FaRegTrashAlt size={15}/> Excluir filme</button>
+                        </div>
+                    </div>
+                    <div className="w-[60%] h-full flex flex-col pl-4">
+                        <div className="w-full flex gap-2 items-baseline">
+                            <h1 className="text-[30px] text-[#9b87f5] font-bold">Bairro 13</h1>
+                            <p className="text-[#8a898c] font-semibold text-[16px]">(2004)</p>
+                        </div>
+                        <div className="w-full gap-4 flex">
+                            <div className="py-1 px-4 rounded-2xl bg-[#4ade80]/20 text-[#4ade80]">
+                                <p className="font-semibold">10/10</p>
+                            </div>
+                            <div className="py-1 px-4 rounded-2xl bg-[#9b87f5]/20 text-[#9b87f5]">
+                                <p className="font-semibold">Ação</p>
+                            </div>
+                        </div>
+                        <div className="w-full flex flex-col pt-4">
+                            <h4 className="font-bold text-[20px] ">Diretor</h4>
+                            <p className="text-[#8a898c]">Pierre Morel</p>
+                            <h4 className="font-bold text-[20px] ">Sinopse</h4>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
            </div>
         </PageWrapper>
     )
